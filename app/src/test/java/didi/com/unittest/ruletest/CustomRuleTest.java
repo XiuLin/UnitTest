fe7d48d2f76609b53d368a65dce0038fe36c262a
xiulin <xiulin@didichuxing.com>
2017-10-26 14:48:56 +0800

    add

--- app/src/test/java/didi/com/unittest/ruletest/CustomRuleTest.java
+++ app/src/test/java/didi/com/unittest/ruletest/CustomRuleTest.java
@@ -1,12 +1,9 @@
 package didi.com.unittest.ruletest;
 
-import android.app.Application;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import didi.com.unittest.BuildConfig;
@@ -18,9 +15,15 @@ import static org.junit.Assert.assertEquals;
  * Created by didi on 2017/10/25.
  */
 
+/**
+ * 不加这些注解会导致RuntimeEnvironment.application为null，为了能给Robolectric必要的环境初始化
+ * 这是遇到的坑之一
+ */
+
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class,packageName = "didi.com.unittest", sdk = 21)
 public class CustomRuleTest {
+
     @Rule
     //public MethodNameExample methodNameExample = new MethodNameExample();
     public ContextRule contextRule = new ContextRule();
@@ -30,7 +33,8 @@ public class CustomRuleTest {
     }
 
     /**
-     * 这里面有个地方需要注意，见ppt最后几页，告诉怎么设置，不然RuntimeEnvironment.application为null
+     * 这里面有个地方需要注意，见ppt最后几页($MODULE_DIR$)，告诉怎么设置，不然会有一些运行错误，
+     * 提示找不到AndroidManifest.xml
      * 这是遇到的坑之一
      */
     @Test
